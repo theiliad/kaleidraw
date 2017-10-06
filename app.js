5e7fa10181ac1aa71865d797bce8fd0b4fc87eb8
The Iliad <iliadmoosavi@gmail.com>
2017-10-06 02:49:49 -0400

    Added the radial brush effect with 36 repeats

--- app.js
+++ app.js
@@ -2,31 +2,63 @@ var w = window,
 d = document,
 e = d.documentElement,
 g = d.getElementsByTagName('body')[0],
-x = w.innerWidth || e.clientWidth || g.clientWidth,
-y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+screenWidth = w.innerWidth || e.clientWidth || g.clientWidth,
+screenHeight = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+var PI = 3.14159265;
 
 var canvas = document.getElementById("main");
-canvas.setAttribute('width', x);
-canvas.setAttribute('height', y);
+canvas.setAttribute('width', screenWidth);
+canvas.setAttribute('height', screenHeight);
 
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FF0000";
 
-function drawPointOnScreen(e) {
+function drawPointOnScreen(x, y) {
+    // var rect = canvas.getBoundingClientRect();
+    // var x = e.clientX - rect.left;
+    // var y = e.clientY - rect.top;
+    // ctx.fillRect(x, y, 3, 3);
+
+    ctx.fillRect(x, y, 2, 2);
+}
+
+function drawRadialPointOnScreen(e) {
     var rect = canvas.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
-    ctx.fillRect(x, y, 3, 3);
+
+    var centerX = (screenWidth / 2);
+    var centerY = (screenHeight / 2);
+
+    var deltaX = x - centerX;
+    var deltaY = y - centerY;
+
+    var radius = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+    var theta = Math.atan((deltaY / deltaX)) * 180 / 3.14159265;
+    var dozent = theta / ((12.0 / 36.0) * 30);
+
+    drawPointOnScreen(x, y);
+
+    for (var i = 0; i < 36; i++) {
+        if (i != dozent) {
+            var newTheta = (i * ((12.0 / 36) * 30)) + theta;
+            var x = centerX + radius * (Math.cos(newTheta * PI / 180));
+            var y = centerY + radius * (Math.sin(newTheta * PI / 180));
+
+            drawPointOnScreen(x, y);
+        }
+    }
 }
 
 canvas.addEventListener("click", function(e) {
-    drawPointOnScreen(e);
+    drawRadialPointOnScreen(e);
 });
 
 canvas.addEventListener("mousedown", function(e) {    
-    canvas.addEventListener("mousemove", drawPointOnScreen);
+    canvas.addEventListener("mousemove", drawRadialPointOnScreen);
 });
 
 canvas.addEventListener("mouseup", function(e) {    
-    canvas.removeEventListener("mousemove", drawPointOnScreen);
+    canvas.removeEventListener("mousemove", drawRadialPointOnScreen);
 });
