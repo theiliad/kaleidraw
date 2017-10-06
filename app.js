f42c37b9f76e0f12ba59aa401a640398aee65f6e
The Iliad <iliadmoosavi@gmail.com>
2017-10-06 02:19:02 -0400

    Can draw points onclick

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+x = w.innerWidth || e.clientWidth || g.clientWidth,
+y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
 var canvas = document.getElementById("main");
+canvas.setAttribute('width', x);
+canvas.setAttribute('height', y);
+
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FF0000";
-ctx.fillRect(0, 0, 150, 75);
+
+document.addEventListener("click", function(e) {
+    var rect = canvas.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+    ctx.fillRect(x, y, 3, 3);
+});
