0e7fa5165d6bce80736f044ad5e8a36c3c96c7f9
The Iliad <iliadmoosavi@gmail.com>
2017-10-06 01:57:35 -0400

    Added canvas and some styling

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-document.querySelector("h1").style.color = "blue";
+var canvas = document.getElementById("main");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0, 0, 150, 75);
